09997b83aaaa6a74aee45f1b1ce1de0cd565f53c
Ramiro Gonzalez <ramiro@xivis.com>
2020-06-22 21:31:35 -0300

    Adding gunicorn

--- app.py
+++ app.py
@@ -3,12 +3,11 @@ from flask import Flask
 from bot import FaucyBot
 
 app = Flask(__name__)
-app.run(host='0.0.0.0', port=environ.get('PORT'))
 
 
 @app.route('/')
 def index():
-    return 'Welcome to Facuy Bot!'
+    return 'Welcome to Faucy Bot!'
 
 
 @app.route('/stats')
@@ -25,3 +24,7 @@ def stats():
         "replies": bot.get_replies()
     }
 
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, host='0.0.0.0', port=environ.get('PORT'))
